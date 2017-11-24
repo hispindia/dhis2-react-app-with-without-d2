f3e04667f9c6be562f8a8ef0feeb60fe260fe924
bsourabh <bsourabh191@gmail.com>
2017-11-24 12:59:17 +0530

    without d2

--- dhis2-react-app/src/index.js
+++ dhis2-react-app/src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Get the root DOM element where the React app will be rendered
+const rootEl = document.querySelector('#root');
+
+// Set the 'Authorization' header to authenticate with the DHIS instance
+const fetchOptions = {
+  headers: {
+    Authorization: `Basic ${btoa('admin:district')}`
+  }
+};
+// Component for displaying a single user
+function User({ user }) {
+  return <div><a href={user.href}>{user.name}</a></div>;
+}
+
+// Component for displaying a list of users
+function UserList({users}) {
+  return (
+    <div>
+      <h3>{users.length} users:</h3>
+      <div>{users.map((user) => <User user={user}/>)}</div>
+    </div>
+  );
+}
+
+// Fetch the list of users
+fetch('https://play.dhis2.org/demo/api/users?fields=:all', fetchOptions)
+  // Parse the results as JSON
+  .then((fetchResult) => fetchResult.json())
+  // Use the data to render a list of users
+  .then((jsonData) => {
+    ReactDOM.render(<UserList users={jsonData.users} />, rootEl);
+  })
+  // Handle errors
+  .catch((error) => { console.warn('Error:', error); });
